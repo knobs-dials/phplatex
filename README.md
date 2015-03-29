db61a189b872814cd55e5fcaef94cdb2db0cfe76
Bart <scarfboy@gmail.com>
2015-03-29 15:57:22 +0200

    initial commit (of v0.6)

--- README.md
+++ README.md
@@ -1,2 +1,79 @@
-# phplatex
-Inline TeX via PHP
+
+Renders LaTeX into images by calling LaTeX itself from PHP.
+
+Requirements
+- PHP                                    (>=4.3.0, as it uses sha1())
+- imagemagick                            (for convert)
+- ghostscript, and TeX Live (or teTeX),  (for latex and dvips)
+- TeX packages: color, amsmath, amsfonts, amssymb, and the extarticle document class
+  Most are standard.   You may need relatively recent versions of some.
+
+
+
+Setup/Installation
+- Have the requirements installed
+  all assumed to be in /usr/bin. You can change the script's reference to them if necessary
+- Put phplatex.php somewhere from which you can include it.
+- In each directory you will be *calling* the script from, create subdirecties 'tmp' and 'images' 
+  with write permissions for the effective user, for example:
+    mkdir tmp images; chown apache:apache tmp images
+- Optional: configure aoache to serve the images with a far-future Expires (since they won't change)
+
+
+Use
+- Include the code:
+    include('path/to/phplatex.php');
+- Each use:
+    echo texify("TeX");
+  This function will return an string containing <img src="...">,
+  which you'll probably PHP-print into the document to show the image.
+
+  Because of PHP, you'll need to double blackslashes, and escape dollar signs
+
+  The function definition is actually:
+    texify(texstring, dpi, r,g,b, br,bg,bb, extraprelude); 
+Maintenance
+- You can empty the tmp directory at will (may sometimes have some leftovers)
+- You can empty the img directory to clean up old images (and have the rest be regenerated on demand)
+
+
+Features
+- Will cache generated images (based on document string), so asking for the same TeX is 
+  trivial (a filesystem check and read). This means you can leave the texify() calls in your code.
+- TeX text used inline in HTML text should show up somewhat decently 
+  (there is some logic looking at character descenders and using CSS lowering to compensate)
+- Allows inclusion of extra TeX packages (via extraprelude)
+- Tweakable size. The default (90) is usually the same size as HTML text.
+  Capped at 300 to avoid resource hogging
+- Allows coloring of page background and default text color
+  (default is black on white, specifically 0.,0.,0. on 1.,1.,1.)
+- Generates PNGs with transparency (note: consider antialiasing to that background)
+- Relies on image trimming instead of trusting dvips' bounding box.
+
+
+Caveats
+- Probably won't work on safe-mode PHP (which is common on much of the cheap shared hosting)
+- Initial generation takes a while, perhaps a second per image
+  On pages with a lot of images you may hit the PHP time limit, 
+  and you'll need a few refreshes before everything is built and cached.
+- Fails when you include TeX that typesets as two pages or more (try \small)
+  (for some reason I'm using landscape. Unless I remember why I'll be removing that,
+   because that just makes things more likely to layout onto two pages)
+- Image conversion fails for very large images  (hence the resolution cap; >300dpi causes easily causes this)
+
+
+Arguables
+- Uses \nonstopmode, meaning latex will fix errors it can, so you can get away with some bad TeX.
+  But it'll guess, and since we delete the log you won't know what it did.
+- You can use arbitrary TeX, which you can see as a feature and as a security issue.
+  Know what this means - and use at your own risk.
+  Mostly (like any other PHP script not running in safe mode), the processes can do everything 
+  that the effective web server user can, so you should have secured your filesystem against that.
+  (if you trust everyone on the server and don't let users input TeX, you're safe enough)
+- Requires recent TeX version as it uses extarticle and includes color, amsmath, amsfont, and amssymb.
+  This may be bothersome for things that aren't recent or not tetex / texlive
+- On low resolutions, the (default) Computer Modern fonts don't render as well as, say, pslatex fonts 
+  (Times, Helvatica, Courier), due to thickness and antialiasing, so change fontset to taste.
+  Sharpening helps only a little, and can look worse when colours or shades are involved 
+  (I tried convert -unsharp 1x1+1+0).
+
---
+++ phplatex.php
@@ -0,0 +1,133 @@
+<?php    
+# Written by scarfboy@gmail.com. 
+# Use at your own risk.
+# See README for usage details.
+
+#In case these are elsewhere
+$path_to_latex   = '/usr/bin/latex';
+$path_to_dvips   = '/usr/bin/dvips';
+$path_to_convert = '/usr/bin/convert';
+
+$imgfmt="png"; #literally used in extensions, and in parameters to convert. Should be either png or gif.
+
+
+function phplatex_cleantmp($tempfname,$todir) {
+  #specifically removes the various files that probably got created for a specific run, based on the run's filename.
+  global $imgfmt;
+  if (chdir($todir)===FALSE) { return '[directory access error, fix permissions (and empty tmp manually this time)]'; }
+  error_reporting(0); #at least one of these probably will not exist, but disable the error reporting related to that.
+  unlink($tempfname);     #the longer/cleaner way would be check for existance for each
+  unlink($tempfname.".tex");  unlink($tempfname.".log");
+  unlink($tempfname.".aux");  unlink($tempfname.".dvi");
+  unlink($tempfname.".ps");   unlink($tempfname.".".$imgfmt);
+  error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
+  #try-catch would have been nice. This is rather overkill too, the way I use it.
+  return '';
+}
+
+function phplatex_colorhex($r,$g,$b) {
+  #there has to be a better way of doing this. It's not even particularly clean.
+  $hex=array("","","");
+  if(strlen($hex[0]=dechex(min(256*$r,255)))==1){ $hex[0]="0".$hex[0]; }
+  if(strlen($hex[1]=dechex(min(256*$g,255)))==1){ $hex[1]="0".$hex[1]; }
+  if(strlen($hex[2]=dechex(min(256*$b,255)))==1){ $hex[2]="0".$hex[2]; }
+  return implode("",$hex);
+}
+
+
+function texify($string,$dpi='90', $r=0.0,$g=0.0,$b=0.0, $br=1.0,$bg=1.0,$bb=1.0,$extraprelude="", $sharpen=FALSE) {
+  global $imgfmt,$path_to_latex,$path_to_dvips,$path_to_convert;
+  if ($dpi>300) $dpi=300;
+
+  $back=phplatex_colorhex($br,$bg,$bb);
+  $fore=phplatex_colorhex($r,$g,$b);
+
+  # Figure out TeX, either to get the right cache entry or to, you know, compile
+  # Semi-common (ams) symbol packages are included.
+  $totex = "\\documentclass[14pt,landscape]{extarticle}\n".
+           "\\usepackage{color}\n".
+           "\\usepackage{amsmath}\n\\usepackage{amsfonts}\n\\usepackage{amssymb}\n".
+           $extraprelude."\n".
+           "\\pagestyle{empty}\n".  #removes header/footer; necessary for trim
+           "\\begin{document}\n".
+           "\\color[rgb]{".$r.",".$g.",".$b."}\n".
+           "\\pagecolor[rgb]{".$br.",".$bg.",".$bb."}\n".
+           $string."\n".
+           "\\end{document}\n";
+  
+  $strhash = sha1($totex).'.'.$dpi; #file cache entry string:  40-char hash string plus size
+  $stralt = str_replace("&","&amp;", preg_replace("/[\"\n]/","",$string)); #stuck in the alt and title attributes
+                                                                           #May need some extra safety.
+  $heredir=getcwd();
+
+  #Experiment: Tries to adjust vertical positioning, so that rendered TeX text looks natural enough inline with HTML text
+  #Only descenders are really a problem since HTML's leeway is upwards.
+  #TODO: This can always use more work. 
+  #      Avoid using characters that are part of TeX commands.
+  #  Some things vary per font, e.g. the slash. In the default CM it is a descender, in Times and others it isn't.
+  $ascenders ="/(b|d|f|h|i|j|k|l|t|A|B|C|D|E|F|G|H|I|J|L|K|M|N|O|P|Q|R|S|T|U|V|W|X|Y|Z|\[|\]|\\{|\\}|\(|\)|\/|0|1|2|3|4|5|6|7|8|9|\\#|\*|\?|'|\\\\'|\\\\`|\\\\v)/";
+  $monoliners="/(a|c|e|m|n|o|r|s|u|v|w|x|z|-|=|\+|:|.)/";
+  $descenders="/(g|j|p|\/|q|y|Q|,|;|\[|\]|\\{|\\}|\(|\)|\#|\\\\LaTeX|\\\\TeX|\\\\c\{)/";
+  $deepdescenders="/(\[|\]|\\{|\\}|\(|\)|\\int)/";
+
+  $ba = preg_match_all($ascenders,  $string,$m); 
+  $bm = preg_match_all($monoliners, $string,$m); 
+  $bd = preg_match_all($descenders, $string,$m); 
+  $dd = preg_match_all($deepdescenders, $string,$m); 
+  if      ($dd>0)            $verticalalign="vertical-align: -25%";   # deep descenders: move down
+  else if ($bd>0 && $ba==0)  $verticalalign="vertical-align: -15%";   # descenders:  move down
+  else if ($bd==0 && $ba>0)  $verticalalign="vertical-align: 0%";     # ascenders only: move up/do nothing?
+  else if ($bd==0 && $ba==0) $verticalalign="vertical-align: 0%";     # neither    vertical-align: 0%
+  else                       $verticalalign="vertical-align: -15%";   # both ascender and regular descender
+
+  #check image cache, return link if exists
+  #the vertical-align is to fix text baseline/descender(/tail) details in and on-average sort of way
+  if (file_exists($heredir.'/images/'.$strhash.'.'.$imgfmt)) 
+    return '<img style="'.$verticalalign.'" title="'.$stralt.'" alt="'.$stralt.'" src="images/'.$strhash.'.'.$imgfmt.'">';
+
+ 
+  #chdir to have superfluous files be created in tmp. (you stiill have to empty this yourself)
+  error_reporting(0); # not checking existence myself, that would be double.
+  if (chdir("tmp")===FALSE) { return '[directory access error, fix permissions]'; } #I should chech whether file creation is allowed to give a nice error for that problem case
+  error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE); # TODO: set old value
+  
+  $tfn = tempnam(getcwd(), 'PTX'); #file in tmp dir
+
+  #write temporary .tex file
+  if ( ($tex = fopen($tfn.'.tex', "w"))==FALSE) { return '[file access error] '.phplatex_cleantmp($tfn,$heredir); }
+  fwrite($tex, $totex); 
+  fclose($tex);
+
+
+  #Run latex to create a .dvi.  Have it try to fix minor errors instead of breaking/pausing on them.
+  exec($path_to_latex.' --interaction=nonstopmode '.$tfn.'.tex');
+  if (!file_exists($tfn.".dvi")) {
+    $log = file_get_contents($tfn.'.log'); #The log always exists, but now it's actually interesting since it'll contain an error
+    return '[latex error, code follows]<pre>'.$totex.'</pre><p><b>Log file:</b><pre>'.$log.'</pre></p> '.phplatex_cleantmp($tfn,$heredir);
+  }
+  
+
+  #DVI -> PostScript.   Since dvips uses lpr, which may be configured to actually print by default, force writing to a file with -o
+  exec($path_to_dvips.' '.$tfn.'.dvi -o '.$tfn.'.ps');
+  if ( !file_exists($tfn.'.ps'))  {
+    return '[dvi2ps error] '.phplatex_cleantmp($tfn,$heredir);
+  }
+
+
+  #PostScript -> image. Trim based on corner pixel, and set transparent color.
+  exec($path_to_convert.' -transparent-color "#'.$back.'" -colorspace RGB -density '.$dpi.' -trim +page '.$tfn.'.ps '.$tfn.'.'.$imgfmt);  
+  #Note: +page OR -page +0+0 OR +repage moves the image to the cropped area (kills offset)
+  #Older code tried: exec('/usr/bin/mogrify -density 90 -trim +page -format $imgfmt '.$tfn.'.ps');
+  # It seems some versions of convert may not have -trim. Old versions?
+
+  if (!file_exists($tfn.'.'.$imgfmt))  {
+    return '[image convert error] '.phplatex_cleantmp($tfn,$heredir);
+  }
+
+  #Copy result image to chache.
+  copy($tfn.'.'.$imgfmt, $heredir.'/images/'.$strhash.'.'.$imgfmt);
+
+  #Clean up temporary files, and return link to just-created image
+  return phplatex_cleantmp($tfn,$heredir).'<img style="'.$verticalalign.'" title="'.$stralt.'" alt="LaTeX formula: '.$stralt.'" src="images/'.$strhash.'.'.$imgfmt.'">';
+} 
+?>
